3eb5839c73f02501ff36e53677ed0f18c2ec4a32
vash <v45xer@gmail.com>
2024-08-29 14:12:53 -0500

    chore: Update NUM_MODE default to Morse Code

--- ui/anduril/config-default.h
+++ ui/anduril/config-default.h
@@ -27,7 +27,7 @@
 #define USE_GLOBALS_CONFIG
 
 // Default number output mode (1 = Digits, 2 = Morse Code)
-#define NUM_MODE 1  // Default to Digits
+#define NUM_MODE 2  // Default to Digits
 
 #if (MCU==0x85) || (MCU==0x1634)
 // sloppy temperature sensor needs bigger error margin
