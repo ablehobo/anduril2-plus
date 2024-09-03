9bf7d183bf9f4adab86441181073f32ac3aff8ec
vash <v45xer@gmail.com>
2024-09-02 22:46:43 -0500

    refactor: Update Morse code decoding to use pre-processed sequences

--- ui/anduril/morse-code.c
+++ ui/anduril/morse-code.c
@@ -7,37 +7,43 @@ uint8_t morse_speed = DEFAULT_MORSE_SPEED;  // Speed in milliseconds
 uint8_t message_length = INVALID_MORSE_CODE; // Default to invalid message so init runs
 
 typedef struct {
-    const char* pattern;
-    uint8_t length;
+    uint8_t pattern;  // Stores the bitfield for the Morse code pattern
+    uint8_t length;   // Number of elements in the Morse code pattern
 } MorseCodePattern;
 
-static const MorseCodePattern morse_patterns[] = {
-    { ".-", 2 },    // A
-    { "-...", 4 },  // B
-    { "-.-.", 4 },  // C
-    { "-..", 3 },   // D
-    { ".", 1 },     // E
-    { "..-.", 4 },  // F
-    { "--.", 3 },   // G
-    { "....", 4 },  // H
-    { "..", 2 },    // I
-    { ".---", 4 },  // J
-    { "-.-", 3 },   // K
-    { ".-..", 4 },  // L
-    { "--", 2 },    // M
-    { "-.", 2 },    // N
-    { "---", 3 },   // O
-    { ".--.", 4 },  // P
-    { "--.-", 4 },  // Q
-    { ".-.", 3 },   // R
-    { "...", 3 },   // S
-    { "-", 1 },     // T
-    { "..-", 3 },   // U
-    { "...-", 4 },  // V
-    { ".--", 3 },   // W
-    { "-..-", 4 },  // X
-    { "-.--", 4 },  // Y
-    { "--..", 4 }   // Z
+typedef struct {
+    uint8_t sequence[4];  // Max length of Morse code is 4 elements
+    uint8_t length;       // Number of elements in the sequence
+} PreProcessedMorseCode;
+
+static const PreProcessedMorseCode morse_code_sequences[] = {
+    {{1, 3}, 2},  // A: ".-"
+    {{3, 1, 1, 1}, 4},  // B: "-..."
+    {{3, 1, 3, 1}, 4},  // C: "-.-."
+    {{3, 1, 1}, 3},  // D: "-.."
+    {{1}, 1},  // E: "."
+    {{1, 1, 3, 1}, 4},  // F: "..-."
+    {{3, 3, 1}, 3},  // G: "--."
+    {{1, 1, 1, 1}, 4},  // H: "...."
+    {{1, 1}, 2},  // I: ".."
+    {{1, 3, 3, 3}, 4},  // J: ".---"
+    {{3, 1, 3}, 3},  // K: "-.-"
+    {{1, 3, 1, 1}, 4},  // L: ".-.."
+    {{3, 3}, 2},  // M: "--"
+    {{3, 1}, 2},  // N: "-."
+    {{3, 3, 3}, 3},  // O: "---"
+    {{1, 3, 3, 1}, 4},  // P: ".--."
+    {{3, 3, 1, 3}, 4},  // Q: "--.-"
+    {{1, 3, 1}, 3},  // R: ".-."
+    {{1, 1, 1}, 3},  // S: "..."
+    {{3}, 1},  // T: "-"
+    {{1, 1, 3}, 3},  // U: "..-"
+    {{1, 1, 1, 3}, 4},  // V: "...-"
+    {{1, 3, 3}, 3},  // W: ".--"
+    {{3, 1, 1, 3}, 4},  // X: "-..-"
+    {{3, 1, 3, 3}, 4},  // Y: "-.--"
+    {{3, 3, 1, 1}, 4},  // Z: "--.."
+    {{0}, 7}  // Space: " " (7 units for a space)
 };
 
 static void blink(uint8_t brightness, uint8_t duration) {
@@ -47,33 +53,34 @@ static void blink(uint8_t brightness, uint8_t duration) {
     nice_delay_ms(morse_speed);  // Inter-element gap
 }
 
-
 void decode_morse_pattern(uint8_t letter_index, uint8_t level) {
-    MorseCodePattern pattern = morse_patterns[letter_index];
+    PreProcessedMorseCode code = morse_code_sequences[letter_index];
 
-    for (int i = 0; i < pattern.length; i++) {
-        if (pattern.pattern[i] == '-') {
-            blink(level, 3);  // Dash
-        } else if (pattern.pattern[i] == '.') {
-            blink(level, 1);  // Dot
+    if (letter_index == 26) {  // Assuming space is mapped to index 26
+        // Handle space: 7 * morse_speed for a space
+        nice_delay_ms(7 * morse_speed);
+    } else {
+        for (int i = 0; i < code.length; i++) {
+            if (code.sequence[i] == 3) {
+                blink(level, 3);  // Dash
+            } else if (code.sequence[i] == 1) {
+                blink(level, 1.5);  // Dot
+            }
+
+            set_level(0);
+            nice_delay_ms(morse_speed);  // Inter-element gap
         }
-        
-        // Insert inter-element delay only if this isn't the last element
-        if (i < pattern.length - 1) {
-            nice_delay_ms(morse_speed);
-        }
+
+        nice_delay_ms(3 * morse_speed);  // Final delay between letters
     }
-
-    // Delay between letters
-    nice_delay_ms(3 * morse_speed);
 }
 
 uint8_t message[MAX_MESSAGE_LENGTH];
 
 void init_message(void) {
-    message[0] = 0; // A
-    message[1] = 1; // B
-    message[2] = 2; // C
+    message[0] = 14; // A
+    message[1] = 15; // B
+    message[2] = 20; // C
     message_length = 3;
 }
 
@@ -96,11 +103,12 @@ void store_morse_code_input(uint8_t presses) {
     }
 }
 
+
 void display_morse_code_message(uint8_t brightness) {
     for (uint8_t i = 0; i < message_length; i++) {
-        decode_morse_pattern(message[i], brightness);
+            decode_morse_pattern(message[i], brightness);
+        }
     }
-}
 
 void set_morse_speed(uint8_t speed) {
     morse_speed = speed;
